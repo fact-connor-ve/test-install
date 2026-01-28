fec756fab42555783652b17725d1cbf3ffa31362
Fact-Connor <Connorv@factivity.com>
2026-01-27 22:04:06 -0500

    updated

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	version = "dev" // set via -ldflags at build time
+)
 
 func main() {
-	fmt.Println("CurrentNewPrint")
+	name := flag.String("name", "World", "name to print")
+	showVersion := flag.Bool("version", false, "print version and exit")
+	quiet := flag.Bool("quiet", false, "suppress output")
+
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Println(version)
+		return
+	}
+
+	if *quiet {
+		return
+	}
+
+	fmt.Printf("Hello, %s\n", *name)
 }
